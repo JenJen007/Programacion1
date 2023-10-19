8d0fea86d9c9acf27637753ffd67c8433bda0cba
JenJen007 <95932218+JenJen007@users.noreply.github.com>
2023-10-19 11:28:03 -0300

    Delete ejemploclase.py

--- ejemploclase.py
+++
@@ -1,8 +0,0 @@
-from funciones import *
-while True:   
-    num = int(input("Ingrese un número: "))
-    if num == 0 :
-        break
-    print(f'La sumatoria de los dígitos ingresados es: {sumatoria(num)}')
-    num += sumatoria(num)
-    print (num)
